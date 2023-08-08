ed906e10c8fbfc6384336fff79541d4609ae6014
drikadev <adriguti.adriana@hotmail.com>
2023-08-07 23:27:00 -0300

    iniciando estudos com python

---
+++ olaMundo.py
@@ -0,0 +1,14 @@
+print("Olá Mundo!")
+#print("Aprendendo Python.")
+print("comentário de linha se faz com #")
+print("comentário de bloco se faz com 3 aspas no início e no fim do bloco")
+
+print(2 + 2)
+
+minha_variavel = "olá mundo"
+print(minha_variavel)
+
+minha_idade = 44
+tenho_filho = True
+print(minha_idade)
+print(tenho_filho)
